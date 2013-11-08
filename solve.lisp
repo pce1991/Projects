4063ec7be0a384645def109c28ec50fce70de224
pce1991 <PCEye@Patricks-MacBook-Air.local>
2013-11-08 01:02:32 -0600

    Edited
    edited

--- solve.lisp
+++ solve.lisp
@@ -14,7 +14,10 @@
         (list (op e) (rhs e) (lhs e))
       (if (and (not (equal (rhs e) v))
                (not (consp (rhs e))))
-          nil))) )
+          nil))))
+  ;now here is where I write a pattern matcher. 
+  ;so this is what I do, return call on solve with a substitution in e
+  ;but how do I do the substituion match. 
 
-(setq algpats 2)
-
+;;do I need a readlist for this or is that just java? 
+(setq algpats (list '( (= ?x (+ ?y ?z)) (= (- ?x ?y) ?z))))
